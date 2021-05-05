362811c8d4d868241567ecafc83255491c52bca7
Michael Verschoof <m.verschoof@btcdirect.eu>
2021-05-05 14:40:24 +0200

    Complete code of the tutorial

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,41 @@
     <router-link to="/about">About</router-link>
   </div>
   <router-view/>
+
+  <div class="demo-container">
+    <animated-component>
+      <div class="demo">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut scelerisque molestie nisl ac suscipit. Maecenas in auctor ligula.
+        Vestibulum suscipit mi eget risus aliquet, id lacinia elit consectetur. Donec a tincidunt justo. Etiam varius urna et finibus lacinia.
+        Vestibulum id auctor urna. Integer non facilisis urna. Nullam dapibus, sem in blandit posuere, sapien sem rhoncus urna, ac placerat massa sem tempor nisi.
+      </div>
+    </animated-component>
+
+    <animated-component animation-type="zoom">
+      <div class="demo">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut scelerisque molestie nisl ac suscipit. Maecenas in auctor ligula.
+        Vestibulum suscipit mi eget risus aliquet, id lacinia elit consectetur. Donec a tincidunt justo. Etiam varius urna et finibus lacinia.
+        Vestibulum id auctor urna. Integer non facilisis urna. Nullam dapibus, sem in blandit posuere, sapien sem rhoncus urna, ac placerat massa sem tempor nisi.
+        <br><br>
+        Vivamus eget nulla ut nisl venenatis lobortis non quis tellus. Nullam eget ornare tortor. Donec ac erat eget massa dapibus condimentum.
+        Nunc sed nisi vel quam facilisis pharetra nec at lectus. Nam ipsum sem, auctor vel varius quis, fringilla non magna. Cras id magna velit.
+        Nam faucibus mi massa, ac posuere nulla elementum at. Duis eget felis vitae nisl aliquet tempor. Vivamus nec porta purus.
+      </div>
+    </animated-component>
+  </div>
+
 </template>
+
+<script lang="js">
+import AnimatedComponent from '@/components/AnimatedComponent.vue';
+
+export default {
+  name: "App",
+  components: {
+    AnimatedComponent
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -26,5 +60,18 @@
       color: #42b983;
     }
   }
+}
+
+.demo {
+  max-width: 600px;
+  border: 2px solid #2c3e50;
+  padding: 2rem;
+  margin: 2rem auto;
+  background-color: #42b983;
+}
+
+.demo-container {
+  min-height: 100vh;
+  padding: 50vh 0;
 }
 </style>
---
+++ src/components/AnimatedComponent.vue
@@ -0,0 +1,75 @@
+<template>
+  <div ref="target">
+    <transition :name="animationType">
+      <div v-if="animate" class="animated-component">
+        <slot />
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script lang="js">
+import { onMounted, ref } from 'vue';
+
+export default {
+  name: 'AnimatedComponent',
+  props: {
+    animationType: {
+      type: String,
+      required: false,
+      default: 'fade'
+    }
+  },
+  setup() {
+    const target = ref();
+    const animate = ref(false);
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        animate.value = entry.isIntersecting;
+      },
+      {
+        threshold: 0.5
+      }
+    );
+
+    onMounted(() => {
+      observer.observe(target.value);
+    });
+
+    return {
+      animate,
+      target
+    };
+  }
+};
+</script>
+
+<style scoped>
+.animated-component.fade-enter-from,
+.animated-component.zoom-enter-from {
+  transition: none;
+}
+
+/* Fade animation */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 300ms ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+/* Zoom animation */
+.zoom-enter-active,
+.zoom-leave-active {
+  transition: transform 300ms ease;
+}
+
+.zoom-enter-from,
+.zoom-leave-to {
+  transform: scale(0.9);
+}
+</style>
