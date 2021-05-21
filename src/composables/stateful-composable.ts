ff955723db81250e4edc4a4a4babeacee5ef0541
Michael Verschoof <m.verschoof@btcdirect.eu>
2021-05-21 17:42:31 +0200

    Added composable with a global and local state that can be updated via a button on the homepage

---
+++ src/composables/stateful-composable.ts
@@ -0,0 +1,30 @@
+import { reactive } from "vue";
+
+const globalState = reactive({
+  someString: "Initial value",
+  someBoolean: false
+});
+
+export const useStatefulComposable = () => {
+
+  const localState = reactive({
+    someString: "Initial value",
+    someBoolean: false
+  });
+
+  const updateValues = (stringValue: string, booleanValue: boolean) => {
+    // Set the global state values
+    globalState.someString = stringValue;
+    globalState.someBoolean = booleanValue;
+
+    // Set the local state values
+    localState.someString = stringValue;
+    localState.someBoolean = booleanValue;
+  };
+
+  return {
+    globalState,
+    localState,
+    updateValues
+  };
+};
